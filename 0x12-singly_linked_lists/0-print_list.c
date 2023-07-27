c6ca9362feefa3e9c2da1023e1f65aad0fba18fb
Rise Code Sleep <risecodesleep@gmail.com>
2023-07-27 16:50:42 -0700

    Pushed all project tasks

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+/*
+ * File: 0-print_list.c
+ * Auth: Rise Code Sleep
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: singly linked list
+ *
+ * Return: the number of nodes printed
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}
